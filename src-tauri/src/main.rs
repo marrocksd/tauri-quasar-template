4fdf577155347632dec7b6229d72a94f4f11695c
Phuoc Nguyen <phuocnguyen@Phuocs-iMac.lan>
2022-12-30 20:01:52 +0700

    init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,12 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+fn main() {
+  app::AppBuilder::new().run();
+
+  // tauri::Builder::default()
+  //   .run(tauri::generate_context!())
+  //   .expect("error while running tauri application");
+}
